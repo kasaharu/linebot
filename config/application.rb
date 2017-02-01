ee6eace87ff4a8946995abaaf23211daac8e6b5b
Wataru.Kasahara <kasahara1019@gmail.com>
2017-02-01 12:47:15 +0000

    EagerLoad を設定

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module Linebot
   class Application < Rails::Application
+    config.paths.add 'lib', eager_load: true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
